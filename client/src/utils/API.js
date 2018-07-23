a649abbd1a71c0baec8c034c61a028e99d9e6e79
Regina Lo <35210714+tlo015@users.noreply.github.com>
2018-07-23 13:58:35 -0700

    added API key

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASEURL = "";
-const APIKEY = "";
+const BASEURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=";
+const APIKEY = "722eb5388c2e47489ad4bd5f397e4d61";
 
 export default {
-    search: function(query) {
-        return axios.get(BASEURL + query + APIKEY);
+    search: function(query, start, end) {
+        return axios.get(BASEURL + APIKEY + "?q=" + query + "?begin_date=" + start + "?end_date=" + end );
     }
 };
