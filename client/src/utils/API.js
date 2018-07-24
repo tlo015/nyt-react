18c3258e6cf84faa68e58d1e7ce978b5ebfe74e0
Regina Lo <35210714+tlo015@users.noreply.github.com>
2018-07-24 16:34:00 -0700

    get, post, delete

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,7 +4,23 @@ const BASEURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-k
 const APIKEY = "722eb5388c2e47489ad4bd5f397e4d61";
 
 export default {
+    //search NYT API
     search: function(query, start, end) {
         return axios.get(BASEURL + APIKEY + "?q=" + query + "?begin_date=" + start + "?end_date=" + end );
+    },
+    
+    //retrieves saved articles 
+    getArticle: function() {
+        return axios.get("/api/saved");
+    },
+    
+    //saves article 
+    saveArticle: function(article) {
+        return axios.post("/api/saved", article);
+    },
+    
+    //delete article 
+    deleteArticle: function(id) {
+        return axios.delete("/api/saved" + id);
     }
 };
