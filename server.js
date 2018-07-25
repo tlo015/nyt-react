6f4cb5a3e5ced76e23c6ebd930c520146e5e42bb
Regina Lo <35210714+tlo015@users.noreply.github.com>
2018-07-25 08:49:23 -0700

    added routes path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -15,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 
 
 // Define API routes here
-// app.use(routes);
+app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
